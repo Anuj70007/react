2ea45ffdfd11b36ecc73e62c7efb392caec66a96
Anuj70007 <62589148+Anuj70007@users.noreply.github.com>
2021-04-04 22:00:59 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
+import React from "react";
+import ReactDom from "react-dom";
+ReactDom.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is new react module</p>
+  </div>,
+  document.getElementById("root")
+);
